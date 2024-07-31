2a8c5f0a9e6ce9eaf6d67bfa6ba3fd55ec6af5e3
Embrialeilani <embria_shanawa_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:55:23 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,10 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Ini file Tes");
+        System.out.println("Halo");
+        System.out.println("Nama: Embria Leilani Shanawa");
+        System.out.println("Almat: Jl.Tri Jaya VIII/195,Kota Madiun");
+        System.out.println("Asal Sekolah: SMPN 13 Madiun");
+        System.out.println("No. Hp: 081252677921");
+     }
+}
